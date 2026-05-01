9a18eb4873792147ff5d244151e6d72249e3b6ec
Marat Bektemirov <0x900x900@gmail.com>
2026-05-01 03:42:54 +0300

    fixed deploy

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import path from "node:path";
+
 export default defineConfig({
   root: __dirname,
-  base: process.env.BASE_PATH ?? "/",
+
+  resolve: {
+    alias: {
+      site: __dirname,
+    },
+  },
+
+  base:
+    process.env.GITHUB_DOMAIN === "yes"
+      ? process.env.BASE_PATH ?? "/"
+      : "/",
 
   build: {
     outDir: path.resolve(__dirname, "../dist-site"),
